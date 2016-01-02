dd7f84bee27a140e9b9ea2a2f25bc117ea94ee56
Scrat1980 <ivazin3@gmail.com>
2016-01-02 12:59:37 +0300

    Index file updated.

--- web/index.php
+++ web/index.php
@@ -1,1 +1,14 @@
+<?php 
+
+require 'controller.php';
+
+// (new Controller())->index();
+
+$controller = new Controller;
+
+if(isset($_GET['action']) && !empty($_GET['action'])){
+	$controller->{$_GET['action']}();
+} else {
+	$controller->index();
+}
 
